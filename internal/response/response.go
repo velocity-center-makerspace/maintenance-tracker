dd7cf833f9407a9ae8e9af10b250b8d1b0357923
Jack Gaskins <jackgaskins.tech@gmail.com>
2026-08-01 18:10:59 -0400

    add custom args to response
    NewWithArgs to not break current New

--- internal/response/response.go
+++ internal/response/response.go
@@ -7,12 +7,20 @@ import (
 )
 
 type Response struct {
-	Msg string `json:"msg"`
+	Msg  string `json:"msg"`
+	Args map[string]any
 }
 
 func New(msg string) *Response {
 	return &Response{
 		Msg: msg,
+	}
+}
+
+func NewWithArgs(msg string, args map[string]any) *Response {
+	return &Response{
+		Msg:  msg,
+		Args: args,
 	}
 }
 
